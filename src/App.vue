6b3ccda8d10689fb018469933fd7550e06dc1ab9
张秋红 <zhangqiuhong@zhangqiuhongdeMacBook-Pro.local>
2022-08-19 23:29:47 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,22 @@
       mode="horizontal"
       @select="handleSelect"
     >
-      <el-menu-item index="1">首页</el-menu-item>
+      <el-menu-item index="1">
+        <RouterLink to="/">首页</RouterLink>
+      </el-menu-item>
       <el-sub-menu index="2">
-        <el-menu-item index="2-1">背景滚动1</el-menu-item>
-        <el-menu-item index="2-2">背景滚动2</el-menu-item>
+        <template #title>背景滚动</template>
+        <el-menu-item index="2-1">
+          <RouterLink to="/demo1">背景滚动1</RouterLink>
+        </el-menu-item>
+        <el-menu-item index="2-2">
+          <RouterLink to="/demo2">背景滚动2</RouterLink>
+        </el-menu-item>
       </el-sub-menu>
     </el-menu>
-    <RouterView />
+    <div class="page-view">
+      <RouterView />
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
@@ -24,65 +33,8 @@ const handleSelect = (key: string, keyPath: string[]) => {
 };
 </script>
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.page-view {
+  padding: 10px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
